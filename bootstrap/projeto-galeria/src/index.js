abbbcf69cd0020cd4a473acaa98057aff0e96075
Lucas Ricardo <lucasricardolemes@gmail.com>
2020-06-18 23:19:56 -0300

    Adicionando controle de Cidades

--- bootstrap/projeto-galeria/src/index.js
+++ bootstrap/projeto-galeria/src/index.js
@@ -1,2 +1,14 @@
-// Arquivo SASS
+// index js é o enter point no web pack para ser trabalhado
+// Arquivo SASS sendo importado,
 import "./scss/index.scss"
+
+
+
+// Dependencias
+import 'jquery' // sem extensao ele supoe que é um arquivo js, caso tenha a extensao ele utiliza aqueles loaders que fizemos com regex para carregar o arquivo.
+import 'bootstrap'
+
+
+//Meus arquivos
+import "./js/core/includes"
+import "./js/plugins/cityButtons"

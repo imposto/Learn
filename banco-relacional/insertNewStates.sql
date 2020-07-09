fc787fe8b9456aec30b8697026aa84f11835c690
Lucas Ricardo <lucasricardolemes@gmail.com>
2020-07-08 21:30:08 -0300

    Banco Relacional

---
+++ banco-relacional/insertNewStates.sql
@@ -0,0 +1,10 @@
+insert into estados (id, nome, sigla, regiao, populacao)
+values (1000, 'Petrolina', 'PT', 'Norte', 1.2)
+
+
+select * from estados 
+
+insert into estados ( nome, sigla, regiao, populacao)
+values (2000, 'Nacarabu', 'NC', 'Norte' 2.2)
+
+select * from estados

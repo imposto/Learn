b55246286e88d4082cdda174f6241e6b32737e60
Lucas Ricardo <lucasricardolemes@gmail.com>
2020-07-10 05:20:21 -0300

    Banco Relacional

---
+++ banco-relacional/deleteStades.sql
@@ -0,0 +1,3 @@
+select id, nome, sigla from estados where id = 1000
+
+delete from estados where sigla = 'PT'
--- banco-relacional/insertNewStates.sql
+++ banco-relacional/insertNewStates.sql
@@ -5,6 +5,6 @@ values (1000, 'Petrolina', 'PT', 'Norte', 1.2)
 select * from estados 
 
 insert into estados ( nome, sigla, regiao, populacao)
-values (2000, 'Nacarabu', 'NC', 'Norte' 2.2)
+values (2000, 'Nacarabu', 'NC', 'Norte', 2.2)
 
 select * from estados

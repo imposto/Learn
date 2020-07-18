7151de2b24c6c9e49763d5c308a8988157e49836
Lucas Ricardo <lucasricardolemes@gmail.com>
2020-07-18 17:14:20 -0300

    Express

--- express/index.js
+++ express/index.js
@@ -1,9 +1,32 @@
 const express = require ('express')
 const app = express()
 
+app.use('/opa', (req, resp, next) =>{
+    console.log('Antes')
+    next()
+})
 
+app.get('/opa', (req, resp, next)=>{
+    resp.json({
+        data: [
+            { "name": 'Ipad 102s',
+            "price": 2833,
+            "discount" : 0.12},
+            {"id": 1, "name": "Analice", "salary": 2833},
+            {"id": 1, "name": "Analice", "salary": 2833},
+            {"id": 1, "name": "Analice", "salary": 2833},
+            {"id": 1, "name": "Analice", "salary": 2833}
+        ]
+    })
+    //res.send('HTML HTMÇ')
+    console.log('durante')
+    next()
+})
 
+app.use('/opa', (req, resp) =>{
+    console.log('Depois')
+})
 
-app.listen(3000, () =>{
+app.listen(3001, () =>{
     console.log('Back eND eXecultando...')
 })
